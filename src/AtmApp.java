ae7bdc5d21633fa0d8770b74bde1456d6bdc2d1f
Vildan Ince <vildanince29@gmail.com>
2023-04-26 11:37:23 +0200

    v8:yeniden_v6

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -1,17 +1,10 @@
 public class AtmApp {
     public static void main(String[] args) {
         System.out.println("bu bir Atm projesidir");
-//versiyon 1
-        System.out.println("java is easy");
-        //versiyon2
 
-        System.out.println("learn java earn money");
-        //versiyon3
-        System.out.println("javayi ogrenmek cok kolay");
-        //versiyon4
 
-        System.out.println("git ogrenmek ne kadar da keyifli ve zevkli");
-        //versiyon6
+
+
 
     }
 }
